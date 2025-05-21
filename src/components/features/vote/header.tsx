4ea31864b0b9431e6ea1a3056358f26415478351
only1Ksy <winterksy@gmail.com>
2025-05-22 01:45:49 +0900

    feat: modify header component

--- src/components/features/vote/header.tsx
+++ src/components/features/vote/header.tsx
@@ -11,7 +11,9 @@ export default function Header() {
     switch (current) {
       case 'list':
         return 0;
-      case 'cast':
+      case 'front':
+      case 'back':
+      case 'demo':
       case 'loading':
         return 1;
       case 'result':
