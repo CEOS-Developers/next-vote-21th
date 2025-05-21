e706220e348294c4ee344a3918eae67cbd1ec9d6
only1Ksy <winterksy@gmail.com>
2025-05-21 23:04:53 +0900

    chore: modify vote layout

--- src/app/vote/list/page.tsx
+++ src/app/vote/list/page.tsx
@@ -2,7 +2,7 @@ export default function VoteList() {
   return (
     <>
       {/* title */}
-      <div className="mt-16 flex justify-center">
+      <div className="mt-16 mb-15 flex justify-center">
         <h1 className="text-headline-03 text-grayscale-00-black">파트장/데모데이 투표</h1>
       </div>
       {/* content */}
