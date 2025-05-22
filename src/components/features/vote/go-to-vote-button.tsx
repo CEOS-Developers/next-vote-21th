228c0b346ce121616c5c7e9c6aaafbe738c70f71
only1Ksy <winterksy@gmail.com>
2025-05-22 11:23:27 +0900

    feat: add result show button in go to vote button component

--- src/components/features/vote/go-to-vote-button.tsx
+++ src/components/features/vote/go-to-vote-button.tsx
@@ -11,21 +11,33 @@ type ButtonProps = {
 export default function GoToVoteButton({ text1, text2, href }: ButtonProps) {
   const router = useRouter();
 
+  const onResultClick = () => {
+    router.push(href + '/result');
+  };
+
   return (
-    <div
-      className="group border-grayscale-00-black bg-grayscale-04 flex h-24 w-74.5 cursor-pointer items-center justify-between border-2"
-      onClick={() => router.push(href)}
-    >
-      <p className="text-headline-04 text-grayscale-00-black group-hover:text-grayscale-00-black ml-10">
+    <div className="border-grayscale-00-black bg-grayscale-04 flex h-39 w-74.5 cursor-pointer justify-between border-2">
+      <p className="text-headline-04 text-grayscale-00-black group-hover:text-grayscale-00-black mt-8 ml-8 self-baseline">
         {text1}
         <br />
         {text2}
       </p>
 
-      <button className="bg-grayscale-03 group-hover:bg-neutral-01 mr-10 flex h-10 w-10 items-center justify-center rounded-full border-2 transition-colors">
-        {/* 체크표시: 그룹에 hover되면 등장 */}
-        <span className="text-xl font-bold text-gray-700 opacity-0 transition-opacity group-hover:opacity-100">✓</span>
-      </button>
+      <div className="mr-6 mb-8 grid items-center justify-items-center gap-4 self-end">
+        <button
+          onClick={() => router.push(href)}
+          className="bg-grayscale-03 hover:bg-neutral-01 flex h-10 w-10 items-center justify-center rounded-full border-2 transition-colors"
+        >
+          {/* 체크표시: 그룹에 hover되면 등장 */}
+          <span className="text-xl font-bold text-gray-700 opacity-0 transition-opacity hover:opacity-100">✓</span>
+        </button>
+        <button
+          onClick={onResultClick}
+          className="text-caption-03 bg-grayscale-03 hover:bg-neutral-01 z-20 flex h-9 w-19.5 items-center justify-center rounded-full border-2 transition-colors"
+        >
+          결과 보기
+        </button>
+      </div>
     </div>
   );
 }
