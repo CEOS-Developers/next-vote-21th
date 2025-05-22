22f74ad71a57e968214ed3981bd8d99cedd41734
only1Ksy <winterksy@gmail.com>
2025-05-22 09:29:40 +0900

    feat: vote cast page onSubmit Button function

--- src/app/vote/[vote-type]/[cast]/page.tsx
+++ src/app/vote/[vote-type]/[cast]/page.tsx
@@ -3,10 +3,12 @@
 import Back from '@components/common/back';
 import CandidateGrid from '@components/features/vote/candidate-grid';
 import { BE, FE, TEAMS } from '@constants/memberData';
-import { useParams } from 'next/navigation';
+import { useVoteStore } from '@stores/vote-store';
+import { useParams, useRouter } from 'next/navigation';
 
 export default function VoteStep2() {
   const params = useParams();
+  const router = useRouter();
 
   // front / back / demo
   const type = params['cast'];
@@ -15,6 +17,27 @@ export default function VoteStep2() {
     if (type === 'front') return FE;
     if (type === 'back') return BE;
     return TEAMS;
+  };
+
+  const { selectedFrontend, selectedBackend, selectedTeam } = useVoteStore();
+
+  const onSubmit = () => {
+    let selected: string | null = null;
+
+    if (type === 'front') selected = selectedFrontend;
+    else if (type === 'back') selected = selectedBackend;
+    else if (type === 'team') selected = selectedTeam;
+
+    if (!selected) {
+      alert('후보를 선택해주세요!');
+      return;
+    }
+
+    // TODO: 나중에 이곳에서 API 요청
+    // await fetch('/api/vote', { method: 'POST', body: JSON.stringify({ voteType, selected }) });
+
+    // 결과 페이지로 이동
+    router.push(`/vote/${params['vote-type']}/result`);
   };
 
   return (
@@ -28,7 +51,10 @@ export default function VoteStep2() {
       <div>
         <CandidateGrid list={getCandidates()} />
         <div className="flex justify-center">
-          <button className="text-caption-01 bg-grayscale-03 mt-4 h-11.5 w-38.5 cursor-pointer rounded-[100px] border-2">
+          <button
+            onClick={onSubmit}
+            className="text-caption-01 bg-grayscale-03 mt-4 h-11.5 w-38.5 cursor-pointer rounded-[100px] border-2"
+          >
             제출하기
           </button>
         </div>
