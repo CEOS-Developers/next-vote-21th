6b1646f43a058dc68ee64fad70fee6e687ba8894
only1Ksy <winterksy@gmail.com>
2025-05-22 10:18:49 +0900

    chore: pull modified code from feat/vote

--- src/components/features/vote/select-vote-button.tsx
+++ src/components/features/vote/select-vote-button.tsx
@@ -10,19 +10,12 @@ type ButtonProps = {
 };
 
 export default function SelectVoteButton({ text, href, type }: ButtonProps) {
-  const voteType = useVoteStore((state) => state.voteType);
   const setVoteType = useVoteStore((state) => state.setVoteType);
   const router = useRouter();
 
-  const isSelected = voteType === type;
-
   const handleClick = () => {
-    if (isSelected) {
-      setVoteType(null); // 해제만 하고 이동하지 않음
-    } else {
-      setVoteType(type); // 타입 저장하고
-      router.push(href); // 페이지 이동
-    }
+    setVoteType(type); // 타입 저장하고
+    router.push(href); // 페이지 이동
   };
 
   return (
@@ -33,10 +26,8 @@ export default function SelectVoteButton({ text, href, type }: ButtonProps) {
       onClick={handleClick}
     >
       <p className="text-headline-04 text-grayscale-00-black group-hover:text-grayscale-00-black ml-10">{text}</p>
-      <button
-        className={`mr-10 h-10 w-10 rounded-full border-2 transition-colors ${isSelected ? 'bg-neutral-01 border-gray-600' : 'bg-grayscale-03 hover:bg-neutral-01'}`}
-      >
-        {isSelected && <span className="text-xl font-bold text-gray-700">✓</span>}
+      <button className="bg-grayscale-03 hover:bg-neutral-01 mr-10 h-10 w-10 rounded-full border-2 transition-colors">
+        <span className="text-xl font-bold text-gray-700 opacity-0 transition-opacity group-hover:opacity-100">✓</span>
       </button>
     </div>
   );
