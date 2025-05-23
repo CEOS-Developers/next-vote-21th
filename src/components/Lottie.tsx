84fd3e69816c8450ef50e2e305e17ff70957eea7
Yeongseo Kim <1903020@ewhain.net>
2025-05-23 23:31:30 +0900

    feat: lottie 컴포넌트 추가

---
+++ src/components/Lottie.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+const Lottie = () => {
+  return (
+    <DotLottieReact
+      src="https://lottie.host/3d761d28-0ea3-4665-9fe2-3ee370978a25/yiVW4lTlbZ.json"
+      loop
+      autoplay
+      speed={0.6}
+    />
+  );
+};
+
+export default Lottie;
