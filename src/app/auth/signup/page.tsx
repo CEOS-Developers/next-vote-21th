aec0b05d089bdc2ab547afeb628c40ef21545d21
DefineXX <keaikim77@gmail.com>
2025-05-24 16:42:32 +0900

    feat: 회원가입 페이지 Form 구현 및 Verify 여부에 따른 조건부 스타일링 적용

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,35 @@
+import AccountPrompt from '@/components/common/auth/AccountPrompt';
+import CustomInput from '@/components/common/auth/CustomInput';
+import SubmitButton from '@/components/common/auth/SubmitButton';
+import VerifyButton from '@/components/common/auth/VerifyButton';
+
+export default function SignupPage() {
+  return (
+    <div className="w-100 h-fit">
+      <h1 className="en-text text-5xl font-normal leading-1.2 mb-8">
+        Create account
+      </h1>
+
+      <form className="w-full h-fit flex flex-col gap-6">
+        <div className="w-full h-fit flex flex-col gap-5">
+          <div className="w-full h-fit relative">
+            <CustomInput
+              type="email"
+              placeholder="Email"
+              isVerifyContained={true}
+            />
+            <VerifyButton />
+          </div>
+          <CustomInput type="password" placeholder="Password" />
+        </div>
+        <SubmitButton buttonText="Get Started" />
+      </form>
+
+      <AccountPrompt
+        prompt="이미 회원이신가요?"
+        linkText="로그인"
+        route="/auth/login"
+      />
+    </div>
+  );
+}
--- src/components/common/auth/CustomInput.tsx
+++ src/components/common/auth/CustomInput.tsx
@@ -1,14 +1,24 @@
+import clsx from 'clsx';
+
 type CustomInputProps = {
   type: 'text' | 'email' | 'password';
   placeholder: string;
+  isVerifyContained?: boolean;
 };
 
-const CustomInput = ({ type, placeholder }: CustomInputProps) => {
+const CustomInput = ({
+  type,
+  placeholder,
+  isVerifyContained = false,
+}: CustomInputProps) => {
   return (
     <div>
       <input
         type={type}
-        className="w-full h-fit py-4 px-4 border-2 border-monochrome-200 rounded-lg text-xl font-normal text-monochrome-black focus:outline-primary-300 placeholder:text-monochrome-300"
+        className={clsx(
+          'w-full h-fit py-4 px-4 border-2 border-monochrome-200 rounded-lg text-xl font-normal text-monochrome-black focus:outline-primary-300 placeholder:text-monochrome-300',
+          isVerifyContained && 'pr-25'
+        )}
         placeholder={placeholder}
       />
     </div>
