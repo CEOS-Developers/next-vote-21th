cb980fe87503ae14416213c6cf5d61d720f1c0fc
DefineXX <keaikim77@gmail.com>
2025-05-24 19:24:16 +0900

    chore: 미사용 import문 삭제

--- src/app/vote/[type]/page.tsx
+++ src/app/vote/[type]/page.tsx
@@ -1,23 +1,19 @@
-import ResultItem from "@/components/features/vote/ResultItem";
-import ResultList from "@/components/features/vote/ResultList";
-import VoteItem from "@/components/features/vote/VoteItem";
-import VoteList from "@/components/features/vote/VoteList";
-import VoteResultTab from "@/components/features/vote/VoteResultTab";
-import Image from "next/image";
+import VoteResultTab from '@/components/features/vote/VoteResultTab';
+import Image from 'next/image';
 
 export default async function Page({
   params,
 }: {
-  params: Promise<{ type: "leader" | "demoday" }>;
+  params: Promise<{ type: 'leader' | 'demoday' }>;
 }) {
   const { type } = await params;
-  const ordinalNumber = type === "leader" ? "22nd" : "21st";
-  const headerContent = type === "leader" ? "Team Leader" : "Demo Day";
+  const ordinalNumber = type === 'leader' ? '22nd' : '21st';
+  const headerContent = type === 'leader' ? 'Team Leader' : 'Demo Day';
   return (
     <div className="flex flex-col items-center gap-4 mt-53 pb-50">
       <header className="text-[40px] en-text flex flex-col items-center">
         CEOS {ordinalNumber} {headerContent} Vote
-        <Image src={"/brush.svg"} alt="브러쉬" width={429} height={9} />
+        <Image src={'/brush.svg'} alt="브러쉬" width={429} height={9} />
       </header>
       <VoteResultTab type={type} />
     </div>
